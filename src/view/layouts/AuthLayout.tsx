020aefffbb1ffeb25cf47ae71a0ec2304252c39e
notFaceroll <overhealzlol@gmail.com>
2023-09-10 22:32:18 -0300

    update: improve auth layout styles and elements

--- src/view/layouts/AuthLayout.tsx
+++ src/view/layouts/AuthLayout.tsx
@@ -1,10 +1,32 @@
 import { Outlet } from "react-router-dom";
+import illustration from "../../assets/illustration.png";
+import { Logo } from "../components/Logo";
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>AuthLayout</h1>
-      <Outlet />
+    <div className="flex w-full h-full">
+      <div className="w-1/2 h-full flex items-center justify-center flex-col gap-16">
+        <Logo className="h-6 text-gray-500" />
+        <div className="w-full max-w-md">
+          <Outlet />
+        </div>
+      </div>
+
+      <div className="w-1/2 h-full flex justify-center itens-center p-8 relative">
+        <img
+          src={illustration}
+          alt="login"
+          className="object-cover w-full max-w-[656px] max-h-[960px] select-none rounded-[32px]"
+        />
+        <div className="max-w-[656px] bg-white absolute bottom-8 rounded-b-[32px] p-10">
+          <Logo className="text-teal-900 h-8" />
+
+          <p className="text-gray-700 font-medium text-xl mt-8">
+            Gerencie suas finanças pessoais de uma forma simples com o fincheck,
+            e o melhor, totalmente de graça!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
