2f5e6457ecaa4dd74cdcaf0f1f48ea1a580cd53a
notFaceroll <overhealzlol@gmail.com>
2023-10-08 09:50:53 -0300

    fix: remote connections

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,23 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { env } from './shared/config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  // app.enableCors({ origin: process.env.FRONTEND_URL });
-  app.enableCors({ origin: '*' });
 
-  await app.listen(3000);
+  app.enableCors({
+    origin: [env.feDevURL, env.feProdURL],
+    // origin: '*',
+    credentials: true,
+  });
+
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+  await app.listen(port, '0.0.0.0').then(() => {
+    console.log(`🔥 HTTP server running at -> ${port}`);
+  });
 }
 bootstrap();
--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -4,23 +4,32 @@ import { IsNotEmpty, IsString, NotEquals, validateSync } from 'class-validator';
 class Env {
   @IsString()
   @IsNotEmpty()
+  dbURL: string;
+
+  @IsString()
+  @IsNotEmpty()
   @NotEquals('unsecure_jwt_secret')
   jwtSecret: string;
 
   @IsString()
   @IsNotEmpty()
-  dbURL: string;
+  feDevURL: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @NotEquals('unsecure_prod_url')
+  feProdURL: string;
 }
 
-// This is the only place where we use process.env directly
-// because we need to load the env variables before the app starts
 export const env: Env = plainToInstance(Env, {
-  jwtSecret: process.env.JWT_SECRET,
   dbURL: process.env.DATABASE_URL,
+  jwtSecret: process.env.JWT_SECRET,
+  feDevURL: process.env.FRONT_END_DEV_URL,
+  feProdURL: process.env.FRONT_END_PROD_URL,
 });
 
 const errors = validateSync(env);
 
 if (errors.length > 0) {
-  throw new Error(JSON.stringify(errors, null, 2));
+  throw new Error(JSON.stringify(errors, null, 4));
 }
